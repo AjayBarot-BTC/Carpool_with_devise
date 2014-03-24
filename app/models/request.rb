ec399d65fb371e9261cc2f7e18fb0acf9619ea8f
AjayBarot <ajubarot@gmail.com>
2014-03-24 10:46:36 +0530

    latest code

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,6 +3,7 @@ class Request < ActiveRecord::Base
 	default_scope -> { order('created_at DESC') }
 	validates :content, presence: true, length: { maximum: 140 }
 	validates :user_id, presence: true
+	validates :destination, presence: true
 	def self.from_users_followed_by(user)
     	followed_user_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id" 
     	where("user_id IN (#{ followed_user_ids }) OR user_id = :user_id", user_id: user.id)
